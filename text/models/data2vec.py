7c14395a4086ccedcb4acd350207bd5059306a48
AryanShekarlaban <arxyzan@gmail.com>
2022-02-04 23:24:30 +0330

    cleanup and naming fix

--- text/models/data2vec.py
+++ text/models/data2vec.py
@@ -8,7 +8,14 @@ from ema import EMA
 
 class Data2VecTextEncoder(nn.Module):
     """
-    Encoder module for Data2Vec model for text
+    Encoder module for Data2Vec model for text.
+
+    This module comprises two main modules. The main encoder model e.g, RoBERTa as the student and an EMA instance
+        of the same student model as the teacher.
+
+    Args:
+        cfg: Config object loaded from a yaml file using OmegaConf. keys and values can be accessed as attributes.
+        encoder (FairseqEncoder): Encoder model
     """
 
     def __init__(self, cfg, encoder: FairseqEncoder, device, mask_idx):
@@ -26,18 +33,18 @@ class Data2VecTextEncoder(nn.Module):
     def _build_teacher(self):
         skip_keys = set()
         if self.cfg.ema_transformer_layers_only:
-            for k, _ in self.sentence_encoder.embed_positions.named_parameters():
+            for k, _ in self.encoder.embed_positions.named_parameters():
                 skip_keys.add(f"embed_tokens.{k}")
-            for k, _ in self.sentence_encoder.embed_positions.named_parameters():
+            for k, _ in self.encoder.embed_positions.named_parameters():
                 skip_keys.add(f"embed_positions.{k}")
-            if self.sentence_encoder.layernorm_embedding is not None:
-                for k, _ in self.sentence_encoder.layernorm_embedding.named_parameters():
+            if self.encoder.layernorm_embedding is not None:
+                for k, _ in self.encoder.layernorm_embedding.named_parameters():
                     skip_keys.add(f"layernorm_embedding.{k}")
-            if self.sentence_encoder.layer_norm is not None:
-                for k, _ in self.sentence_encoder.layer_norm.named_parameters():
+            if self.encoder.layer_norm is not None:
+                for k, _ in self.encoder.layer_norm.named_parameters():
                     skip_keys.add(f"layernorm_embedding.{k}")
 
-        ema = EMA(self.sentence_encoder, self.cfg, self.device, skip_keys=skip_keys)
+        ema = EMA(self.encoder, self.cfg, self.device, skip_keys=skip_keys)
         return ema
 
     def _build_regression_head(self):
@@ -66,58 +73,33 @@ class Data2VecTextEncoder(nn.Module):
             pct_remaining = 1 - curr_step / total_steps
             return end - r * pct_remaining
 
-        if self.training:
-            if self.cfg.ema_decay != self.cfg.ema_end_decay:
-                if num_updates >= self.cfg.ema_anneal_end_step:
-                    decay = self.cfg.ema_end_decay
-                else:
-                    decay = _get_annealed_rate(self.cfg.ema_decay,
-                                               self.cfg.ema_end_decay,
-                                               num_updates,
-                                               self.cfg.ema_anneal_end_step)
-                self.teacher.decay = decay
-            if self.teacher.decay < 1:
-                self.teacher.step(self.sentence_encoder)
+        if self.cfg.ema_decay != self.cfg.ema_end_decay:
+            if num_updates >= self.cfg.ema_anneal_end_step:
+                decay = self.cfg.ema_end_decay
+            else:
+                decay = _get_annealed_rate(self.cfg.ema_decay,
+                                           self.cfg.ema_end_decay,
+                                           num_updates,
+                                           self.cfg.ema_anneal_end_step)
+            self.teacher.decay = decay
+        if self.teacher.decay < 1:
+            self.teacher.step(self.encoder)
 
     def forward(self, src_tokens, target_tokens=None, features_only=False, return_all_hiddens=False):
 
         x, extra = self.encoder.extract_features(src_tokens, return_all_hiddens=return_all_hiddens)
         if features_only:
             return x, extra
+
         with torch.no_grad():
             self.teacher.model.eval()
             encoder_out = self.teacher.model(target_tokens, return_all_hiddens=True)
             y = encoder_out["fc_results"]
-
             y = y[-self.average_top_k_layers:]
-
-            permuted = False
-            if self.cfg.instance_norm_target_layer or self.cfg.batch_norm_target_layer:
-                y = [tl.permute(1, 2, 0) for tl in y]  # TBC -> BCT
-                permuted = True
-
-            if self.cfg.batch_norm_target_layer:
-                y = [F.batch_norm(tl.float(), running_mean=None, running_var=None, training=True) for tl in y]
-
-            if self.cfg.instance_norm_target_layer:
-                y = [F.instance_norm(tl.float()) for tl in y]
-
-            if permuted:
-                y = [tl.transpose(1, 2) for tl in y]  # BCT -> BTC
-
-            if self.cfg.layer_norm_target_layer:
-                y = [F.layer_norm(tl.float(), tl.shape[-1:]) for tl in y]
-
+            y = [F.layer_norm(tl.float(), tl.shape[-1:]) for tl in y]
             y = sum(y) / len(y)
-
-            if not permuted:
-                y = y.transpose(0, 1)
-
-            if self.cfg.layer_norm_targets:
-                y = F.layer_norm(y.float(), y.shape[-1:])
-
-            if self.cfg.instance_norm_targets:
-                y = F.instance_norm(y.transpose(1, 2)).transpose(1, 2)
+            y = y.transpose(0, 1)
+            y = F.layer_norm(y.float(), y.shape[-1:])
 
         masked_indices = src_tokens.eq(self.mask_idx)
 
@@ -126,7 +108,6 @@ class Data2VecTextEncoder(nn.Module):
 
         x = self.regression_head(x)
 
-        N = x.size(-1)
         if self.cfg.loss_beta == 0:
             loss = F.mse_loss(x.float(), y.float(), reduction="none").sum(dim=-1)
         else:
@@ -134,7 +115,7 @@ class Data2VecTextEncoder(nn.Module):
 
         result = {
             "losses": {
-                "main": loss.sum() / math.sqrt(N) if self.loss_scale <= 0 else loss.sum() * self.loss_scale
+                "main": loss.sum() / math.sqrt(x.size(-1)) if self.loss_scale <= 0 else loss.sum() * self.loss_scale
             },
             "sample_size": loss.numel(),
         }
@@ -142,4 +123,8 @@ class Data2VecTextEncoder(nn.Module):
         # logging other values
         other_logs = {"ema_decay": self.teacher.decay * 1000}
         result["logs"] = other_logs
+
         return result
+
+class Data2VecModel(nn.Module):
+    ...
