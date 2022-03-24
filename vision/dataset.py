524669804ba6f0ebeaba6c622daf992e48ef1016
AryanShekarlaban <arxyzan@gmail.com>
2022-03-24 13:26:29 +0430

    [vision] fix data construction for train step

--- vision/dataset.py
+++ vision/dataset.py
@@ -1,38 +1,39 @@
 import torch
 from torchvision.datasets import ImageFolder
 
-from .transform import MIMTransform
+from .transforms import MIMTransform
 
 
-class BEiTPretrainingDataset(ImageFolder):
+class MIMPretrainingDataset(ImageFolder):
 
     def __init__(self, cfg, **kwargs):
-        super(BEiTPretrainingDataset, self).__init__(root=cfg.dataset.path)
-        self.transform = MIMTransform(cfg)
+        super(MIMPretrainingDataset, self).__init__(root=cfg.dataset.path)
+        self.transform = MIMTransform(cfg.dataset)
+        self.input_size = cfg.dataset.input_size
         self.device = cfg.device
-        self.mask_token_id = 0.
-        self.pad_token_id = 1.
         self.__dict__.update(kwargs)
 
     def __getitem__(self, index):
         # TODO figure out how the source and target must be constructed
         path, target = self.samples[index]
         image = self.loader(path)
-        image, masked_image, bool_masked_pos = self.transform(image)
-        return image, masked_image, bool_masked_pos
+        image, mask = self.transform(image)
+        mask = mask.reshape(1, 14, 14, 1, 1)
+        image = image.reshape(-1, 14, 14, 16, 16)
+        masked_image = (image * mask).reshape(-1, self.input_size, self.input_size)
+        target_image = (image * ~mask).reshape(-1, self.input_size, self.input_size)
+        return masked_image, target_image, mask.reshape(14, 14)
 
 
 if __name__ == '__main__':
-    from dall_e import load_model
     from omegaconf import OmegaConf
     from transformers import BeitModel, BeitConfig
     from torch.utils.data import DataLoader
 
     model = BeitModel(BeitConfig())
-    d_vae = load_model('encoder.pkl')
     cfg = OmegaConf.load('configs/beit-pretraining.yaml')
     cfg.dataset.path = 'dummy_data'
-    dataset = BEiTPretrainingDataset(cfg)
+    dataset = MIMPretrainingDataset(cfg)
     loader = DataLoader(dataset, batch_size=4)
     src, trg = next(iter(loader))
     print(src)
---
+++ vision/transforms.py
@@ -0,0 +1,138 @@
+"""
+Transforms and masking strategies for Self Supervised Pretraining of Vision models.
+All codes copied from below repos:
+https://github.com/microsoft/unilm/tree/master/beit
+https://github.com/rwightman/pytorch-image-models/tree/master/timm
+https://github.com/facebookresearch/deit
+"""
+import warnings
+import random
+import math
+from PIL import Image
+import numpy as np
+import torch
+from torchvision import transforms
+import torchvision.transforms.functional as F
+from timm.data.constants import (IMAGENET_DEFAULT_MEAN, IMAGENET_DEFAULT_STD,
+                                 IMAGENET_INCEPTION_MEAN, IMAGENET_INCEPTION_STD)
+from timm.data.transforms import RandomResizedCropAndInterpolation
+
+_pil_interpolation_to_str = {
+    Image.NEAREST: 'PIL.Image.NEAREST',
+    Image.BILINEAR: 'PIL.Image.BILINEAR',
+    Image.BICUBIC: 'PIL.Image.BICUBIC',
+    Image.LANCZOS: 'PIL.Image.LANCZOS',
+    Image.HAMMING: 'PIL.Image.HAMMING',
+    Image.BOX: 'PIL.Image.BOX',
+}
+
+
+def _pil_interp(method):
+    if method == 'bicubic':
+        return Image.BICUBIC
+    elif method == 'lanczos':
+        return Image.LANCZOS
+    elif method == 'hamming':
+        return Image.HAMMING
+    else:
+        # default bilinear, do we want to allow nearest?
+        return Image.BILINEAR
+
+
+_RANDOM_INTERPOLATION = (Image.BILINEAR, Image.BICUBIC)
+
+
+class MIMTransform(object):
+    """
+    Masked Image Modeling transforms based on BEiT. copied from https://github.com/microsoft/unilm/tree/master/beit
+    """
+
+    def __init__(self, cfg):
+        imagenet_default_mean_and_std = cfg.imagenet_default_mean_and_std
+        mean = IMAGENET_INCEPTION_MEAN if not imagenet_default_mean_and_std else IMAGENET_DEFAULT_MEAN
+        std = IMAGENET_INCEPTION_STD if not imagenet_default_mean_and_std else IMAGENET_DEFAULT_STD
+        patch_size = cfg.patch_size
+        num_patches = cfg.num_patches
+        assert patch_size * num_patches == cfg.input_size
+
+        self.common_transform = transforms.Compose([
+            transforms.ColorJitter(0.4, 0.4, 0.4),
+            transforms.RandomHorizontalFlip(p=0.5),
+            RandomResizedCropAndInterpolation(size=cfg.input_size, interpolation=cfg.interpolation),
+            transforms.ToTensor(),
+            transforms.Normalize(mean=torch.tensor(mean), std=torch.tensor(std))
+        ])
+
+        self.masked_position_generator = MaskingGenerator(
+            cfg.num_patches, num_masking_patches=cfg.num_mask_patches,
+            max_num_patches=cfg.max_mask_patches_per_block,
+            min_num_patches=cfg.min_mask_patches_per_block,
+        )
+
+    def __call__(self, image):
+        return self.common_transform(image), self.masked_position_generator()
+
+
+class MaskingGenerator:
+    def __init__(
+            self, input_size, num_masking_patches, min_num_patches=4, max_num_patches=None,
+            min_aspect=0.3, max_aspect=None):
+        if not isinstance(input_size, tuple):
+            input_size = (input_size,) * 2
+        self.height, self.width = input_size
+
+        self.num_masking_patches = num_masking_patches
+
+        self.min_num_patches = min_num_patches
+        self.max_num_patches = num_masking_patches if max_num_patches is None else max_num_patches
+
+        max_aspect = max_aspect or 1 / min_aspect
+        self.log_aspect_ratio = (math.log(min_aspect), math.log(max_aspect))
+
+    def __repr__(self):
+        repr_str = "Generator(%d, %d -> [%d ~ %d], max = %d, %.3f ~ %.3f)" % (
+            self.height, self.width, self.min_num_patches, self.max_num_patches,
+            self.num_masking_patches, self.log_aspect_ratio[0], self.log_aspect_ratio[1])
+        return repr_str
+
+    def get_shape(self):
+        return self.height, self.width
+
+    def _mask(self, mask, max_mask_patches):
+        delta = 0
+        for attempt in range(10):
+            target_area = random.uniform(self.min_num_patches, max_mask_patches)
+            aspect_ratio = math.exp(random.uniform(*self.log_aspect_ratio))
+            h = int(round(math.sqrt(target_area * aspect_ratio)))
+            w = int(round(math.sqrt(target_area / aspect_ratio)))
+            if w < self.width and h < self.height:
+                top = random.randint(0, self.height - h)
+                left = random.randint(0, self.width - w)
+
+                num_masked = mask[top: top + h, left: left + w].sum()
+                # Overlap
+                if 0 < h * w - num_masked <= max_mask_patches:
+                    for i in range(top, top + h):
+                        for j in range(left, left + w):
+                            if mask[i, j] == 0:
+                                mask[i, j] = 1
+                                delta += 1
+
+                if delta > 0:
+                    break
+        return delta
+
+    def __call__(self):
+        mask = torch.zeros(self.get_shape(), dtype=torch.int)
+        mask_count = 0
+        while mask_count < self.num_masking_patches:
+            max_mask_patches = self.num_masking_patches - mask_count
+            max_mask_patches = min(max_mask_patches, self.max_num_patches)
+
+            delta = self._mask(mask, max_mask_patches)
+            if delta == 0:
+                break
+            else:
+                mask_count += delta
+
+        return mask
